3c078dc2bdee50e6e65ad8ccc0b02928d8de4a88
Paulo Henrique Nonaka <paulononaka@gmail.com>
2020-02-25 20:20:48 -0300

    Reindent code

--- python_scrapy/spiders/spider.py
+++ python_scrapy/spiders/spider.py
@@ -14,14 +14,12 @@ class FaraSpider(scrapy.Spider):
         # inspect_response(response, self)
 
         p_instance = response.xpath('//input[@id="pInstance"]/@value').get()
-        p_request = 'PLUGIN=' + \
-            response.xpath(
-                '/html/body/script[2]/text()',
-            ).re(r'ajaxIdentifier":"(.*)"')[1]
-        p_json = '{"pageItems":null,"salt":"' + \
-            response.xpath(
-                '//input[@id="pPageSubmissionId"]/@value',
-            ).get() + '"}'
+        p_request = response.xpath(
+            '/html/body/script[2]/text()',
+        ).re(r'ajaxIdentifier":"(.*)"')[1]
+        p_page_submission_id = response.xpath(
+            '//input[@id="pPageSubmissionId"]/@value',
+        ).get()
 
         return FormRequest(
             url='https://efile.fara.gov/ords/wwv_flow.ajax',
@@ -29,7 +27,7 @@ class FaraSpider(scrapy.Spider):
                 'p_flow_id': '171',
                 'p_flow_step_id': '130',
                 'p_instance': p_instance,
-                'p_request': p_request,
+                'p_request': 'PLUGIN=' + p_request,
                 'p_widget_name': 'worksheet',
                 'p_widget_mod': 'ACTION',
                 'p_widget_action': 'BREAK',
@@ -37,7 +35,7 @@ class FaraSpider(scrapy.Spider):
                 'x01': '80340213897823017',
                 'x02': '80341508791823021',
                 'x03': 'COUNTRY_NAME',
-                'p_json': p_json,
+                'p_json': '{"pageItems":null,"salt":"' + p_page_submission_id + '"}',
             },
             callback=self.after_repage,
         )
